7ba8609432b3e7ef6e4963d72a2a12b6b4afb382
hungvv2808 <vuhung2881999@gmail.com>
2021-02-08 17:31:59 +0700

    fix le_connector

---
+++ pub/le_connector/db_custom.php
@@ -0,0 +1,13 @@
+<?php
+return array(
+    'cart_type' => 'magento',
+    'db_host' => 'magentomysql',
+    'db_user' => 'root',
+    'db_password' => 'root',
+    'db_name' => 'magento',
+    'db_prefix' => '',
+    'image_dir' => '',
+    'image_dir_product' => '',
+    'image_dir_category' => '',
+    'version' => '',
+);
